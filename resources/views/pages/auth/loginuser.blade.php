b0c3f7e33a8c7f898a60119a959819533c526910
RanggaPrathama <ranggaprathama9@gmail.com>
2023-12-12 10:58:04 +0700

    update admin

---
+++ resources/views/pages/auth/loginuser.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="{{ asset('css/loginuser.css') }}" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loginuser.css"> -->
+    <title>Login Page | loginpage</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="loginuser.css" />
+</head>
+
+<body>
+    <div class="login-user">
+        <div class="div">
+            <div class="overlap">
+                <div class="group">
+                    <div>
+                        <input class="email" placeholder="username" type="text" name="email"/>
+                        <input class="password" placeholder="password" type="password" name="password"/>
+                        </div>
+
+                        <div>
+                            <a href="homepage.html"><button class="sign-in" type="submit">Login</button></a>
+                            </div>
+
+                </div>
+                <img class="removal" src="img/Logo Vokasi.png" />
+                <div class="admisi-vokasi">
+                    <a href="data_mahasiswa.html"><div class="overlap-group"></div>
+                        <div class="text-wrapper">Admisi VOKASI</div>
+                        <img class="icon-lock" src="img/kunci.png" />
+                    </div>
+                </div>
+            </div>
+            <img class="img" src="img/Logo UB.png" />
+            <img class="MASTER" src="img/logo Kampus merdeka.png" />
+            <div class="overlap-2">
+                <img class="removal-2" src="img/Logo user.png" />
+                <p class="kampus-merdeka">
+                    Kampus Merdeka adalah bagian dari kebijakan Merdeka Belajar oleh Kementerian <br />Pendidikan, Kebudayaan,
+                    Riset, dan Teknologi (Kemendikbudristek) yang<br />memberikan seluruh mahasiswa kesempatan untuk mengasah
+                    <br />kemampuan sesuai bakat dan minat dengan terjun langsung ke <br />dunia kerja sebagai langkah persiapan
+                    karier.
+                </p>
+            </div>
+            <div class="overlap-3">
+                <div class="text-wrapper-2">Kampus Merdeka</div>
+                <p class="program-persiapan">
+                    Program persiapan karier yang komprehensif <br />untuk mempersiapkan generasi terbaik Indonesia
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
